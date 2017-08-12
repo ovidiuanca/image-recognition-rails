4117bc85cbed1a28da6e8aaebb4644e8e454e6c6
Ovidiu Cota <ovidiu@MacBook-Pro-3.local>
2017-08-12 16:14:01 +0300

    Adds einstein vision generate token service

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @token = EinsteinVision::GenerateToken.new.execute
   end
 end
---
+++ app/services/einstein_vision/generate_token.rb
@@ -0,0 +1,41 @@
+require 'jwt'
+require 'rest-client'
+require 'json'
+
+module EinsteinVision
+  class GenerateToken
+    def execute
+      ps_endpoint = ENV['EINSTEIN_VISION_URL']
+      subject = ENV['EINSTEIN_VISION_ACCOUNT_ID']
+      private_key = String.new(ENV['EINSTEIN_VISION_PRIVATE_KEY'])
+      private_key.gsub!('\n', "\n")
+      expiry = Time.now.to_i + (60 * 15)
+
+      # Read the private key string as Ruby RSA private key
+      rsa_private = OpenSSL::PKey::RSA.new(private_key)
+
+      # Build the JWT payload
+      payload = {
+              :sub => subject,
+              :aud => "https://api.einstein.ai/v2/oauth2/token",
+              :exp => expiry
+          }
+
+      # Sign the JWT payload
+      assertion = JWT.encode payload, rsa_private, 'RS256'
+      puts assertion
+
+      # Call the OAuth endpoint to generate a token
+      response = RestClient.post(ps_endpoint + 'v2/oauth2/token', {
+              grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+              assertion: assertion
+          })
+
+      token_json = JSON.parse(response)
+      puts "\nGenerated access token:\n"
+      puts JSON.pretty_generate(token_json)
+
+      access_token = token_json["access_token"]
+    end
+  end
+end
